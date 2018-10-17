af9582f0bc7515b346bfb5acfaabdf6adfaec81c
mekwall <marcus.ekwall@gmail.com>
2018-10-17 13:14:19 +0200

    feat: expose all functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
 export { isInstallNeeded } from './isInstallNeeded';
+export { writeCheckFile } from './writeCheckFile';
+export { findClosestFile } from './findClosestFile';
+export { fileHash } from './fileHash';
